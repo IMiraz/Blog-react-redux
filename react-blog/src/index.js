9a9e5da23ee9e099b55ee549dbd9f9a70a8190dc
wasifmiraz <wasifmiraz@mail.com>
2018-10-01 17:11:40 +0600

    setting up react-router

--- react-blog/src/index.js
+++ react-blog/src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route}  from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const about = () => {
+     return <h1>this is about</h1>
+}
+
+
+const contact = () => {
+    return <h1>this is contact</h1>
+}
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+ <Route exact path="/" component={App}/>
+ <Route path="/about" component={about}/>
+ <Route path="/contact" component={contact} />
+        </div>
+    </BrowserRouter>
+
+
+    , document.getElementById('root'));
 registerServiceWorker();
