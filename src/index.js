ec7a97eb94a7354ec72789f8b15b652bb7315381
wasifmiraz <wasifmiraz@mail.com>
2018-10-01 17:27:35 +0600

    Navigating page with react router link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route}  from 'react-router-dom'
+import {BrowserRouter, Route, Link}  from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -18,6 +18,11 @@ const contact = () => {
 ReactDOM.render(
     <BrowserRouter>
         <div>
+  <ul>
+   <li><Link to="/">root page</Link></li>
+   <li><Link to="/about">about page</Link></li>
+   <li> <Link to="/contact">contact page</Link></li>
+  </ul>
  <Route exact path="/" component={App}/>
  <Route path="/about" component={about}/>
  <Route path="/contact" component={contact} />
