cbbec508cdb7cc71d82d599de5388c3a7006f5ab
wasifmiraz <wasifmiraz@mail.com>
2018-10-02 13:21:28 +0600

    created welcome page paginations

--- src/Components/Welcome/index.jsx
+++ src/Components/Welcome/index.jsx
@@ -17,6 +17,20 @@ const Welcome = () => {
       <div className="row">
         <div className="col-12 col-lg-6 offset-lg-3">
 <Article/>
+<hr/>
+<Article/>
+<hr/>
+<Article/>
+<hr/>
+<Article/>
+<nav className="flexbox mt-50 mb-50">
+  <a className="btn btn-white disabled">
+    <i className="ti-arrow-left fs-9 mr-4" /> Newer</a>
+  <a className="btn btn-white" href="#">Older
+    <i className="ti-arrow-right fs-9 ml-4" />
+  </a>
+</nav>
+
         </div>
         </div>
 </main>
