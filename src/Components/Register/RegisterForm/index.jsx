c9ccbae4a16e96d6bc371f9653da207f6169291c
wasifmiraz <wasifmiraz@mail.com>
2018-10-03 15:32:11 +0600

    refactoring signup form component

---
+++ src/Components/Register/RegisterForm/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+const RegisterForm = ({ handerInputChange, handlerSubmit, errors }) => {
+
+     return (
+        <div className="mh-fullscreen bg-img center-vh p-20" style={{backgroundImage: 'url(assets/img/bg-girl.jpg)'}}>
+        <div className="card card-shadowed p-50 w-400 mb-0" style={{maxWidth: '100%'}}>
+          <h5 className="text-uppercase text-center">Signup</h5>
+          <br />
+          <br />
+          <form className="form-type-material" onSubmit={handlerSubmit}>
+            <div className="form-group">
+              <input type="text" name="name" onChange={handerInputChange} className="form-control" placeholder="Username" />
+
+              {
+                errors.name &&
+                <small className="text-danger">{errors.name}</small>
+
+              }
+            </div>
+            <div className="form-group">
+              <input type="text" name="email" onChange={this.handerInputChange}
+             className="form-control" placeholder="Email address" />
+
+
+             {
+              errors.email &&
+              <small className="text-danger">{errors.email}</small>
+
+            }
+
+            </div>
+            <div className="form-group">
+              <input type="password" onChange={this.handerInputChange} name="password" className="form-control" placeholder="Password" />
+
+              {
+                errors.password&&
+                <small className="text-danger">{errors.password}</small>
+
+              }
+            </div>
+            <div className="form-group">
+              <input type="password" onChange={handerInputChange} name="password_confirmation" className="form-control" placeholder="Password (confirm)" />
+            </div>
+            <br />
+            <button className="btn btn-bold btn-block btn-primary" type="submit">Register</button>
+          </form>
+          <hr className="w-30" />
+          <p className="text-center text-muted fs-13 mt-20">Already have an account?
+            <Link to="/login">Sign in</Link>
+          </p>
+        </div>
+      </div>
+
+     )
+
+}
+
+export default RegisterForm
--- src/Components/Register/index.jsx
+++ src/Components/Register/index.jsx
@@ -2,8 +2,8 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import {validateAll} from 'indicative'
 import axios  from  'axios'
-
 import config from '../config'
+import RegisterForm from  './RegisterForm'
 
 class Signup extends Component {
 
@@ -46,54 +46,13 @@ event.preventDefault();
 
   render (){
       return (
-        <div className="mh-fullscreen bg-img center-vh p-20" style={{backgroundImage: 'url(assets/img/bg-girl.jpg)'}}>
-        <div className="card card-shadowed p-50 w-400 mb-0" style={{maxWidth: '100%'}}>
-          <h5 className="text-uppercase text-center">Signup</h5>
-          <br />
-          <br />
-          <form className="form-type-material" onSubmit={this.handlerSubmit}>
-            <div className="form-group">
-              <input type="text" name="name" onChange={this.handerInputChange} className="form-control" placeholder="Username" />
+         <RegisterForm
+         handerInputChange={this.handerInputChange}
+         handlerSubmit={this.handlerSubmit}
+         errors={this.state.errors}
 
-              {
-                this.state.errors['name'] &&
-                <small className="text-danger">{this.state.errors['name']}</small>
+         />
 
-              }
-            </div>
-            <div className="form-group">
-              <input type="text" name="email" onChange={this.handerInputChange}
-             className="form-control" placeholder="Email address" />
-
-
-             {
-              this.state.errors['email'] &&
-              <small className="text-danger">{this.state.errors['email']}</small>
-
-            }
-
-            </div>
-            <div className="form-group">
-              <input type="password" onChange={this.handerInputChange} name="password" className="form-control" placeholder="Password" />
-
-              {
-                this.state.errors['password'] &&
-                <small className="text-danger">{this.state.errors['password']}</small>
-
-              }
-            </div>
-            <div className="form-group">
-              <input type="password" onChange={this.handerInputChange} name="password_confirmation" className="form-control" placeholder="Password (confirm)" />
-            </div>
-            <br />
-            <button className="btn btn-bold btn-block btn-primary" type="submit">Register</button>
-          </form>
-          <hr className="w-30" />
-          <p className="text-center text-muted fs-13 mt-20">Already have an account?
-            <Link to="/login">Sign in</Link>
-          </p>
-        </div>
-      </div>
       )
   }
 
